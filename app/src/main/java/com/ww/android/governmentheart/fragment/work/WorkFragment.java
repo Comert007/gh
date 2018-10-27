95e119b9797a5634be285e7191bc12191c708309
lipengfeng <sourfeng@163.com>
2018-10-27 14:02:28 +0800

    提交

---
+++ app/src/main/java/com/ww/android/governmentheart/fragment/work/WorkFragment.java
@@ -0,0 +1,20 @@
+package com.ww.android.governmentheart.fragment.work;
+
+import com.ww.android.governmentheart.R;
+import com.ww.android.governmentheart.fragment.BaseFragment;
+import com.ww.android.governmentheart.mvp.model.VoidModel;
+import com.ww.android.governmentheart.mvp.vu.base.VoidView;
+
+public class WorkFragment extends BaseFragment<VoidView, VoidModel> {
+
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_work;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
